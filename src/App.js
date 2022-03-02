e71a0d6da033d6eaa9ff896ce24ecbc84d154fc2
bautiercoli <bautistaercoli7@gmail.com>
2022-03-01 22:35:39 -0300

    desafio 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ItemListContainer from './components/contenedor/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 import NavBar from './components/NavBar/NavBar';
 
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = ({destacados, productos, oferta}) => {
 
+    const onAdd = (count) =>{
+        console.log(`Se añadio al carrito ${count} productos`)
+    }
     return (
         <div className="contenedor">
             <h1>{ destacados }</h1>
             <h2>{ productos }</h2>
+            <ItemCount stock={7} initial={1} onAdd={onAdd} />
             <h2>{ oferta }</h2>
         </div>
     )
 }
 
+
 export default ItemListContainer
+
+
+
+
